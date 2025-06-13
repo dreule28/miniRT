de20bf994271d5df9d9a9122f0ef16fb79b3a361
dreule <danielreule28@gmail.com>
2025-06-13 20:01:58 +0200

    FEAT(parser.c): refactor object handling by adding search_for_objects function and updating light addition

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@ bool	check_argc(int argc)
 	{
 		if (argc == 1)
 		{
-			ft_putstr_fd("Error\nMissing scene!\n",2 );
+			ft_putstr_fd("Error\nMissing scene!\n", 2);
 			return (false);
 		}
 		else
@@ -38,34 +38,43 @@ bool	check_elements(t_scene *scene, char *parsed_line)
 	if (!ft_strncmp(parsed_line, "C", 1))
 		add_camera(scene, parsed_line);
 	if (!ft_strncmp(parsed_line, "L", 1))
-		add_light(scene, parsed_line);
-	// if (!ft_strncmp(parsed_line, "pl", 2))
-	// 	add_plane(scene, parsed_line);
-	// if (!ft_strncmp(parsed_line, "sp", 2))
-	// 	add_sphere(scene, parsed_line);
-	// if (!ft_strncmp(parsed_line, "cy", 2))
-	// 	add_cylinder(scene, parsed_line);
+		add_light_node(scene->light_list, parsed_line);
 	return (true);
+}
+
+void	search_for_objects(t_scene *scene, char *parsed_line)
+{
+	t_obj_node	*new_node;
+
+	if (!ft_strncmp(parsed_line, "pl", 2))
+	{
+		new_node = add_obj_node(scene->obj_list, PLANE);
+		add_plane(new_node, parsed_line);
+	}
+	if (!ft_strncmp(parsed_line, "sp", 2))
+	{
+		new_node = add_obj_node(scene->obj_list, SPHERE);
+		add_sphere(new_node, parsed_line);
+	}
+	if (!ft_strncmp(parsed_line, "cy", 2))
+	{
+		new_node = add_obj_node(scene->obj_list, CYLINDER);
+		add_cylinder(new_node, parsed_line);
+	}
 }
 
 bool	parser(t_scene *scene, int argc, char **argv)
 {
-	int			rt_file;
-	char		*parsed_line;
-	int			count_pl;
-	int			count_sp;
-	int			count_cy;
-	t_obj_list	*obj_list;
-
-	count_pl = 0;
-	count_sp = 0;
-	count_cy = 0;
+	int		rt_file;
+	char	*parsed_line;
 
 	if (!check_argc(argc) || !check_argv(argv))
 		return (false);
 	rt_file = open(argv[1], O_RDONLY);
 	if (rt_file == -1)
-	return (false);
+		return (false);
+	if (!init_lists(&scene->obj_list, &scene->light_list))
+		return (close(rt_file), false);
 	parsed_line = get_next_line(rt_file);
 	while (parsed_line)
 	{
@@ -73,23 +82,12 @@ bool	parser(t_scene *scene, int argc, char **argv)
 		{
 			free(parsed_line);
 			parsed_line = get_next_line(rt_file);
-			continue;
+			continue ;
 		}
-		if (!ft_strncmp(parsed_line, "pl", 2))
-			count_pl++;
-		else if (!ft_strncmp(parsed_line, "sp", 2))
-			count_sp++;
-		else if (!ft_strncmp(parsed_line, "cy", 2))
-			count_cy++;
+		search_for_objects(scene, parsed_line);
 		check_elements(scene, parsed_line);
-		// printf("%s", parsed_line);
 		free(parsed_line);
 		parsed_line = get_next_line(rt_file);
 	}
-	obj_list = init_obj_list();
-	printf("pl: %d\nsp: %d\ncy: %d\n", count_pl, count_sp, count_cy);
-	printf("Ambient - Ratio: (%.1f), RGB: (%.1f,%.1f,%.1f)\n", scene->ambi_light.ratio, scene->ambi_light.rgb.r, scene->ambi_light.rgb.g, scene->ambi_light.rgb.b);
-	printf("Camera - Viewpoint: (%.1f,%.1f,%.1f), Orientation: (%.1f,%.1f,%.1f), FOV: (%d)\n", scene->camera.view_point.x, scene->camera.view_point.y, scene->camera.view_point.z, scene->camera.orientation_vector.x, scene->camera.orientation_vector.y, scene->camera.orientation_vector.z, scene->camera.fov);
-	printf("Light - Position: (%.1f,%.1f,%.1f), Brightness: (%.1f), RGB: (%.1f,%.1f,%.1f)\n", scene->light.light_point.x, scene->light.light_point.y, scene->light.light_point.z, scene->light.light_ratio, scene->light.rgb.r, scene->light.rgb.g, scene->light.rgb.b);
-	return (true);
+	return (close(rt_file), true);
 }
