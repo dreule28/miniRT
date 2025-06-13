5c7865fd0db97031a2d35ff2e123febe48897b67
dreule <danielreule28@gmail.com>
2025-06-13 20:01:30 +0200

    FEAT(list_and_nodes.c): add functions to initialize and add nodes for object and light lists

--- src/parser/list_and_nodes.c
+++ src/parser/list_and_nodes.c
@@ -13,4 +13,74 @@ t_obj_list	*init_obj_list(void)
 	return (obj_list);
 }
 
-t_obj_node	*add_obj_node()
+t_obj_node	*add_obj_node(t_obj_list *obj_list, int obj_type)
+{
+	t_obj_node	*new_node;
+
+	new_node = malloc(sizeof(t_obj_node));
+	if (!new_node)
+		return (NULL);
+	new_node->next = NULL;
+	new_node->type = obj_type;
+	new_node->data = init_obj_data(obj_type);
+	if (!new_node->data)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	if (!obj_list->head)
+		obj_list->head = new_node;
+	else
+		obj_list->tail->next = new_node;
+	obj_list->tail = new_node;
+	obj_list->size++;
+	return (new_node);
+}
+
+t_obj_data	*init_obj_data(int obj_type)
+{
+	t_obj_data	*new_data;
+
+	new_data = malloc(sizeof(t_obj_data));
+	if (!new_data)
+		return (NULL);
+	if (obj_type == SPHERE)
+		return (init_sphere_data(new_data));
+	else if (obj_type == PLANE)
+		return (init_plane_data(new_data));
+	else if (obj_type == CYLINDER)
+		return (init_cylinder_data(new_data));
+	else
+		return (free(new_data), NULL);
+}
+
+t_light_list	*init_light_list(void)
+{
+	t_light_list	*light_list;
+
+	light_list = malloc(sizeof(t_light_list));
+	if (!light_list)
+		return (NULL);
+	light_list->head = NULL;
+	light_list->tail = NULL;
+	light_list->size = 0;
+	return (light_list);
+}
+
+t_light	*add_light_node(t_light_list *light_list, char *parsed_line)
+{
+	t_light	*new_node;
+
+	new_node = malloc(sizeof(t_light));
+	if (!new_node)
+		return (NULL);
+	add_light(new_node, parsed_line);
+	new_node->next = NULL;
+	if (!light_list->head)
+		light_list->head = new_node;
+	else
+		light_list->tail->next = new_node;
+	light_list->tail = new_node;
+	light_list->size++;
+	return (new_node);
+}
