903c43d03c11c062ed8e74ccb443a1c17eadc527
dreule <danielreule28@gmail.com>
2025-06-13 19:59:49 +0200

    FIX(fill_elements.c):  update light parsing to use light struct

--- src/parser/fill_elements.c
+++ src/parser/fill_elements.c
@@ -23,7 +23,6 @@ void	skip_spaces_or_value(char **parsed_line, int value, int comma)
 
 void	add_ambient(t_scene *scene, char *parsed_line)
 {
-	DEBUG_TRACE("Parsing ambient light from: %s", parsed_line);
 	skip_spaces_or_value(&parsed_line, 1, 0);
 	skip_spaces_or_value(&parsed_line, 0, 0);
 	scene->ambi_light.ratio = ft_atof(parsed_line);
@@ -33,14 +32,10 @@ void	add_ambient(t_scene *scene, char *parsed_line)
 	scene->ambi_light.rgb.g = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 1);
 	scene->ambi_light.rgb.b = ft_atof(parsed_line);
-	DEBUG_INFO("Ambient light parsed - Ratio: %.2f, RGB: (%.3f, %.3f, %.3f)",
-		scene->ambi_light.ratio,
-		scene->ambi_light.rgb.r, scene->ambi_light.rgb.g, scene->ambi_light.rgb.b);
 }
 
 void	add_camera(t_scene *scene, char *parsed_line)
 {
-	DEBUG_TRACE("Parsing camera from: %s", parsed_line);
 	skip_spaces_or_value(&parsed_line, 1, 0);
 	skip_spaces_or_value(&parsed_line, 0, 0);
 	scene->camera.pos.x = ft_atof(parsed_line);
@@ -56,33 +51,24 @@ void	add_camera(t_scene *scene, char *parsed_line)
 	scene->camera.orientation_vector.z = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 1, 0);
 	scene->camera.fov = ft_atoi(parsed_line);
-	DEBUG_INFO("Camera parsed - Position: (%.3f, %.3f, %.3f), Orientation: (%.3f, %.3f, %.3f), FOV: %d",
-		scene->camera.pos.x, scene->camera.pos.y, scene->camera.pos.z,
-		scene->camera.orientation_vector.x, scene->camera.orientation_vector.y, scene->camera.orientation_vector.z,
-		scene->camera.fov);
 }
 
-void	add_light(t_scene *scene, char *parsed_line)
+void	add_light(t_light *light, char *parsed_line)
 {
-	DEBUG_TRACE("Parsing light from: %s", parsed_line);
 	skip_spaces_or_value(&parsed_line, 1, 0);
 	skip_spaces_or_value(&parsed_line, 0, 0);
-	scene->light.pos.x = ft_atof(parsed_line);
+	light->pos.x = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 1);
-	scene->light.pos.y = ft_atof(parsed_line);
+	light->pos.y = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 1);
-	scene->light.pos.z = ft_atof(parsed_line);
+	light->pos.z = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 1, 0);
-	scene->light.brightness = ft_atof(parsed_line);
+	light->brightness = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 0);
 	skip_spaces_or_value(&parsed_line, 1, 0);
-	scene->light.rgb.r = ft_atof(parsed_line);
+	light->rgb.r = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 1);
-	scene->light.rgb.g = ft_atof(parsed_line);
+	light->rgb.g = ft_atof(parsed_line);
 	skip_spaces_or_value(&parsed_line, 0, 1);
-	scene->light.rgb.b = ft_atof(parsed_line);
-	DEBUG_INFO("Light parsed - Position: (%.3f, %.3f, %.3f), Brightness: %.2f, RGB: (%.3f, %.3f, %.3f)",
-		scene->light.pos.x, scene->light.pos.y, scene->light.pos.z,
-		scene->light.brightness,
-		scene->light.rgb.r, scene->light.rgb.g, scene->light.rgb.b);
+	light->rgb.b = ft_atof(parsed_line);
 }
