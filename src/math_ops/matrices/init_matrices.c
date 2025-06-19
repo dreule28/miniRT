2a088eb5d202055692a9d89f716998a14b280e93
dreule <danielreule28@gmail.com>
2025-06-19 17:13:03 +0200

    NORM(init_matrices.c): improve code formatting for consistency and readability

--- src/math_ops/matrices/init_matrices.c
+++ src/math_ops/matrices/init_matrices.c
@@ -2,39 +2,39 @@
 
 t_m2	*init_m2(void)
 {
-	t_m2 *m2;
+	t_m2	*m2;
 
 	m2 = ft_calloc(1, sizeof(t_m2));
-	if(!m2)
-		return(NULL);
+	if (!m2)
+		return (NULL);
 	m2->head = NULL;
 	m2->tail = NULL;
 	m2->size = 0;
-	return(m2);
+	return (m2);
 }
 
 t_m3	*init_m3(void)
 {
-	t_m3 *m3;
+	t_m3	*m3;
 
 	m3 = ft_calloc(1, sizeof(t_m3));
-	if(!m3)
-		return(NULL);
+	if (!m3)
+		return (NULL);
 	m3->head = NULL;
 	m3->tail = NULL;
 	m3->size = 0;
-	return(m3);
+	return (m3);
 }
 
 t_m4	*init_m4(void)
 {
-	t_m4 *m4;
+	t_m4	*m4;
 
 	m4 = ft_calloc(1, sizeof(t_m4));
-	if(!m4)
-		return(NULL);
+	if (!m4)
+		return (NULL);
 	m4->head = NULL;
 	m4->tail = NULL;
 	m4->size = 0;
-	return(m4);
+	return (m4);
 }
