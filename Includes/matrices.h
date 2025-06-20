97f2e905592e81c668bf52122f0b8ff1153adb18
dreule <danielreule28@gmail.com>
2025-06-20 13:39:27 +0200

    REFAC(matrices): rename and restructure matrix column types for clarity and consistency

--- Includes/matrices.h
+++ Includes/matrices.h
@@ -5,55 +5,57 @@
 
 typedef struct s_m2_col
 {
-	double			x;
-	double			y;
-	struct s_m2_col	*next;
-}					t_m2_col;
+	double	x;
+	double	y;
+}			t_m2_col;
 
 typedef struct s_m3_col
 {
-	double			x;
-	double			y;
-	double			z;
-	struct s_m3_col	*next;
-}					t_m3_col;
+	double	x;
+	double	y;
+	double	z;
+}			t_m3_col;
 
 typedef struct s_m4_col
 {
-	double			x;
-	double			y;
-	double			z;
-	double			w;
-	struct s_m4_col	*next;
-}					t_m4_col;
+	double	x;
+	double	y;
+	double	z;
+	int		w;
+}			t_m4_col;
 
 typedef struct s_m2
 {
-	t_m2_col	*head;
-	t_m2_col	*tail;
-	ssize_t		size;
+	t_m2_col	*col1;
+	t_m2_col	*col2;
 }				t_m2;
 
 typedef struct s_m3
 {
-	t_m3_col	*head;
-	t_m3_col	*tail;
-	ssize_t		size;
+	t_m3_col	*col1;
+	t_m3_col	*col2;
+	t_m3_col	*col3;
 }				t_m3;
 
 typedef struct s_m4
 {
-	t_m4_col	*head;
-	t_m4_col	*tail;
-	ssize_t		size;
+	t_m4_col	*col1;
+	t_m4_col	*col2;
+	t_m4_col	*col3;
+	t_m4_col	*col4;
 }				t_m4;
 
 //add_col.c -- BEGIN
-t_m2_col	*add_m2_col(t_m2 *m2, double x, double y);
-t_m3_col	*add_m3_col(t_m3 *m3, double x, double y, double z);
-t_m4_col	*add_m4_col(double x, double y, double z, double w);
-void		append_col(t_m4 *m4, t_m4_col *new_col);
+t_m2_col	*assign_m2_col(double x, double y);
+t_m3_col	*assign_m3_col(double x, double y, double z);
+t_m4_col	*assign_m4_col(double x, double y, double z, int w);
 //add_col.c -- END
+
+//cmp_col.c -- BEGIN
+bool	cmp_m2_col(t_m2_col *col1, t_m2_col *col2);
+bool	cmp_m3_col(t_m3_col *col1, t_m3_col *col2);
+bool	cmp_m4_col(t_m4_col *col1, t_m4_col *col2);
+//cmp_col.c -- END
 
 
 //init_matrices.c -- BEGIN
--- src/math_ops/matrices/init_matrices.c
+++ src/math_ops/matrices/init_matrices.c
@@ -7,9 +7,8 @@ t_m2	*init_m2(void)
 	m2 = ft_calloc(1, sizeof(t_m2));
 	if (!m2)
 		return (NULL);
-	m2->head = NULL;
-	m2->tail = NULL;
-	m2->size = 0;
+	m2->col1 = NULL;
+	m2->col2 = NULL;
 	return (m2);
 }
 
@@ -20,9 +19,9 @@ t_m3	*init_m3(void)
 	m3 = ft_calloc(1, sizeof(t_m3));
 	if (!m3)
 		return (NULL);
-	m3->head = NULL;
-	m3->tail = NULL;
-	m3->size = 0;
+	m3->col1 = NULL;
+	m3->col2 = NULL;
+	m3->col3 = NULL;
 	return (m3);
 }
 
@@ -33,8 +32,9 @@ t_m4	*init_m4(void)
 	m4 = ft_calloc(1, sizeof(t_m4));
 	if (!m4)
 		return (NULL);
-	m4->head = NULL;
-	m4->tail = NULL;
-	m4->size = 0;
+	m4->col1 =  NULL;
+	m4->col2 =  NULL;
+	m4->col3 =  NULL;
+	m4->col4 =  NULL;
 	return (m4);
 }
