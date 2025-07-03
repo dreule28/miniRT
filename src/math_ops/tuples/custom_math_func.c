d1a0e2d046845172fd3164084959c170ba7eea7b
dreule <danielreule28@gmail.com>
2025-07-03 12:31:35 +0200

    FEAT(custom_math_func.c): add functions to create vectors, points, and RGB colors

---
+++ src/math_ops/tuples/custom_math_func.c
@@ -0,0 +1,43 @@
+#include "mini_rt.h"
+
+t_tuples *create_vector(double x ,double y ,double z)
+{
+	t_tuples *vector;
+
+	vector = ft_calloc(1, sizeof(t_tuples));
+	if(!vector)
+		return(NULL);
+	vector->x = x;
+	vector->y = y;
+	vector->z = z;
+	vector->w = VECTOR;
+	return(vector);
+}
+
+t_tuples *create_point(double x ,double y ,double z)
+{
+	t_tuples *point;
+
+	point = ft_calloc(1, sizeof(t_tuples));
+	if(!point)
+		return(NULL);
+	point->x = x;
+	point->y = y;
+	point->z = z;
+	point->w = POINT;
+	return(point);
+}
+
+t_rgb *create_rgb(double r ,double g ,double b)
+{
+	t_rgb *rgb;
+
+	rgb = ft_calloc(1, sizeof(t_rgb));
+	if(!rgb)
+		return(NULL);
+	rgb->r = r;
+	rgb->g = g;
+	rgb->b = b;
+	return(rgb);
+}
+
