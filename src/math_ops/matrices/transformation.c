c1830cd3b4797e743ef2b5353773fad6ddd6c361
dreule <danielreule28@gmail.com>
2025-07-03 14:26:20 +0200

    FEAT(transformation.c): add functions for translation and scaling of matrices

---
+++ src/math_ops/matrices/transformation.c
@@ -0,0 +1,19 @@
+#include "mini_rt.h"
+
+t_m4	*translation(t_m4 *matrix, t_tuples *tuple)
+{
+	matrix->col3 = assign_m4_col(matrix->col3->x + tuple->x,
+			matrix->col3->y + tuple->y,
+			matrix->col3->z + tuple->z, 1);
+	return (matrix);
+}
+
+t_m4	*scaling(t_m4 *matrix, t_tuples *tuple)
+{
+	matrix->col0->x = matrix->col0->x * tuple->x;
+	matrix->col1->y = matrix->col1->y * tuple->y;
+	matrix->col2->z = matrix->col2->z * tuple->z;
+	return (matrix);
+}
+
+
