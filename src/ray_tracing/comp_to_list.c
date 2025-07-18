b5ec49cb8a9740968ca6c2df00398a1b569ca155
dreule <danielreule28@gmail.com>
2025-07-18 17:08:14 +0200

    FEAT(comp_to_list.c): added epsilon offset to prevent acne effect

--- src/ray_tracing/comp_to_list.c
+++ src/ray_tracing/comp_to_list.c
@@ -1,10 +1,30 @@
 #include "mini_rt.h"
 
-bool	set_comp_to_obj(t_obj_node *curr, t_ray *ray)
+void	set_dot_product(t_obj_node *curr)
 {
-	double		dot_product;
+	double	dot_product;
+
+	dot_product = ftm_tup_dot(curr->comp->normalv, curr->comp->eyev);
+	if (dot_product < 0)
+	{
+		curr->comp->inside = true;
+		curr->comp->normalv = ftm_tup_negate(curr->comp->normalv);
+	}
+	else
+		curr->comp->inside = false;
+}
+
+void	set_epsilon_offset(t_obj_node *curr)
+{
 	t_tuples	*epsilon_offset;
 
+	epsilon_offset = ftm_tup_mult(curr->comp->normalv, DBL_EPSILON);
+	curr->comp->over_point = ftm_tup_add(curr->comp->point, epsilon_offset);
+	free(epsilon_offset);
+}
+
+bool	set_comp_to_obj(t_obj_node *curr, t_ray *ray)
+{
 	if (!curr->t)
 		return (true);
 	curr->comp->point = ray_position(ray, curr->t[0]);
@@ -19,16 +39,7 @@ bool	set_comp_to_obj(t_obj_node *curr, t_ray *ray)
 		if (!curr->comp->normalv)
 			return (false);
 	}
-	dot_product = ftm_tup_dot(curr->comp->normalv, curr->comp->eyev);
-	if (dot_product < 0)
-	{
-		curr->comp->inside = true;
-		curr->comp->normalv = ftm_tup_negate(curr->comp->normalv);
-	}
-	else
-		curr->comp->inside = false;
-	epsilon_offset = ftm_tup_mult(curr->comp->normalv, DBL_EPSILON);
-	curr->comp->over_point = ftm_tup_add(curr->comp->point, epsilon_offset);
-	free(epsilon_offset);
+	set_dot_product(curr);
+	set_epsilon_offset(curr);
 	return (true);
 }
