4ebec9ca0ff2fc3769d7ddf80075c06cb28be86e
dreule <danielreule28@gmail.com>
2025-08-01 16:49:04 +0200

    FIX(refracted_color.c): correct direction calculation in find_direction and enhance refracted_color logic

--- src/ray_tracing/refracted_color.c
+++ src/ray_tracing/refracted_color.c
@@ -28,7 +28,7 @@ t_tuples	*find_direction(t_computations *comps, double sin2_t)
 	cos_t = sqrt(1.0 - sin2_t);
 	cos_i = ftm_tup_dot(comps->eyev, comps->normalv);
 	n_ratio = comps->n1 / comps->n2;
-	direction = ftm_tup_subtract(ftm_tup_mult(comps->over_point,
+	direction = ftm_tup_subtract(ftm_tup_mult(comps->normalv,
 										(n_ratio * cos_i - cos_t)),
 									ftm_tup_mult(comps->eyev, n_ratio));
 	return (direction);
@@ -42,6 +42,7 @@ t_rgb	*refracted_color(t_obj_node *obj_node, t_obj_list *obj_list,
 	t_tuples	*direction;
 	t_ray		*refract_ray;
 	t_rgb		*color;
+	t_obj_list	*refract_intersections;
 
 	if (remaining == 0)
 		return (init_rgb(0, 0, 0));
@@ -53,7 +54,23 @@ t_rgb	*refracted_color(t_obj_node *obj_node, t_obj_list *obj_list,
 		return (init_rgb(0, 0, 0));
 	direction = find_direction(obj_node->comp, sin2_t);
 	refract_ray = init_ray(obj_node->comp->under_point, direction);
-	(void)refract_ray;
+	refract_intersections = intersect_to_list(scene, refract_ray);
+
+	if (refract_intersections->head && refract_intersections->head->t)
+	{
+		if (set_comp_to_obj(refract_intersections->head, refract_ray, refract_intersections))
+		{
+			color = get_shaded_with_shadows(scene, refract_intersections->head, remaining - 1);
+		}
+		else
+		{
+			color = init_rgb(0, 0, 0);
+		}
+	}
+	else
+	{
+		color = init_rgb(0, 0, 0);
+	}
 	color = get_shaded_with_shadows(scene, obj_node, remaining - 1);
 	color = ftm_rgb_mult(color, material.transparency);
 	return (color);
